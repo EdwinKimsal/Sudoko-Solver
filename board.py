9a4355ae323ce745b428e7b3bbf4cf1bebcba3c8
Edwin Kimsal <kimsal.edwin@gmail.com>
2024-12-27 11:58:18 -0500

    Updated random board generator (still does not work, stuck in infinite loop)

--- board.py
+++ board.py
@@ -1,56 +1,65 @@
 # Import(s)
 import random
 
-# Random list function
-def rand_board(square_len, min_num, max_num):
-    # Set a blank list
-    list = []
-
-    # Iterate for square_len times
-    for x in range(square_len):
-        # Append a list of square_len nums
-        list.append([str(random.randint(min_num, max_num)) for num in range(square_len)])
-
-    # Iterate through each block
-    for i in range(len(list)):
-        for j in range(len(list[i])):
-            # Make 0s spaces
-            if list[i][j] == "0":
-                list[i][j] = " "
-
-    # Return the final list
-    return list
-
-
 # check_board function
 def check_board(board):
     # Iterate through each block in board (left -> right, top -> down)
     for y in range(len(board)):
         for x in range(len(board)):
             # Check horizontal row
-            if board[y][x] in board[y][x+1:]:
-                return False
+            if len(board[y]) > x+1:
+                if board[y][x] in board[y][x+1:]:
+                    return False
 
             # Check vertical row using temp list
             temp = []
             for i in range(len(board) - y - 1):
-                temp.append(board[i + y + 1][x])
+                if len(board[i + y + 1]) > x:
+                    temp.append(board[i + y + 1][x])
 
-            if board[y][x] in temp:
-                return False
+            if len(board[y]) > x:
+                if board[y][x] in temp:
+                    return False
 
             # Check block of nine using temp list
             temp = []
             for i in range(3 - (y % 3)):
                 for j in range(3 - (x % 3)):
-                    if i != 0 or j != 0:
-                        temp.append(board[y+i][x+j])
+                    if len(board[y+i]) > x + j:
+                        if i != 0 or j != 0:
+                            temp.append(board[y+i][x+j])
 
-            if board[y][x] in temp:
-                return False
+            if len(board[y]) > x:
+                if board[y][x] in temp:
+                    return False
 
     # If no return, board is valid
     return True
+
+
+# Random list function
+def rand_board():
+    # Set a blank temp and list
+    list = [[],[],[],[],[],[],[],[],[]]
+
+    # Iterate for square_len times
+    for y in range(9):
+        for x in range(9):
+            isValid = False
+
+            while isValid == False:
+                num = str(random.randrange(1, 10))
+                list[y].append(num)
+                print(list)
+
+                isValid = check_board(list)
+                print(isValid)
+
+                if isValid == False:
+                    list[y].pop(-1)
+
+    # Return the final list
+    return list
 
 
 # Print the board function
@@ -66,20 +75,20 @@ def print_board(board):
 
 # Main function
 def main():
-    # # Generate a random list
-    # list = rand_board(9, 0, 9)
+    # Generate a random list
+    list = rand_board()
 
-    # Set board
-    list = [
-[6, 2, 5, 8, 4, 3, 7, 9, 1],
-[7, 9, 1, 2, 6, 5, 4, 8, 3],
-[4, 8, 3, 9, 7, 1, 6, 2, 5],
-[8, 1, 4, 5, 9, 7, 2, 3, 6],
-[2, 3, 6, 1, 8, 4, 9, 5, 7],
-[9, 5, 7, 3, 2, 6, 8, 1, 4],
-[5, 6, 9, 4, 3, 2, 1, 7, 8],
-[3, 4, 2, 7, 1, 8, 5, 6, 9],
-[1, 7, 8, 6, 5, 9, 3, 4, 2]]
+#     # Set board
+#     list = [
+# [6, 2, 5, 8, 4, 3, 7, 9, 1],
+# [7, 9, 1, 2, 6, 5, 4, 8, 3],
+# [4, 8, 3, 9, 7, 1, 6, 2, 5],
+# [8, 1, 4, 5, 9, 7, 2, 3, 6],
+# [2, 3, 6, 1, 8, 4, 9, 5, 7],
+# [9, 5, 7, 3, 2, 6, 8, 1, 4],
+# [5, 6, 9, 4, 3, 2, 1, 7, 8],
+# [3, 4, 2, 7, 1, 8, 5, 6, 9],
+# [1, 7, 8, 6, 5, 9, 3, 4, 2]]
 
     # Set str_board
     str_board = [[str(int) for int in row] for row in list]
